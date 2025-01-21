3d0d900dbfcba596782739edcabc598b98018556
Roy24-prog <ritobroto19@gmail.com>
2025-01-21 17:11:12 +0530

    Add files via upload

---
+++ Practice/prac.py
@@ -0,0 +1,12 @@
+# adding 2 numbers
+a = int(input("Enter a number: "))
+b = int(input("Enter second number: "))
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+print("Add: ", add)
+print ("Sub: ", sub)
+print ("Mul: ", mul)
+print ("Div: ", div)
+input("Press over...")
